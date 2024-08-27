935a60ff9870a35578005fb46e3155ffc985b431
Derick Eddington <z6Mko5mivMvNtheBHB67cSJeMG6BbCn7Xk6MKY9VegkAbgMH>
2024-08-27 00:25:30 -0700

    Minor adjustment for newer macOS.
    This is now back to how I'd had it before.  Older 10.13 High Sierra needed the previous adjustment
    for the non-standard errno code, but that must've been a bug that they later fixed.

--- tests/usage/named.rs
+++ tests/usage/named.rs
@@ -44,13 +44,8 @@ fn basic() {
 
     let r = Semaphore::unlink(name);
     assert!(r.is_err());
-    let (exp_errno, exp_kind) = if cfg!(target_os = "macos") {
-        (libc::EINVAL, io::ErrorKind::InvalidInput)
-    } else {
-        (libc::ENOENT, io::ErrorKind::NotFound)
-    };
-    assert_eq!(errno(), exp_errno);
-    assert_eq!(r.map_errno().unwrap_err().kind(), exp_kind);
+    assert_eq!(errno(), libc::ENOENT);
+    assert_eq!(r.map_errno().unwrap_err().kind(), io::ErrorKind::NotFound);
 
     drop([s2, s3]);
     // SAFETY: There are no other instances now (because we just dropped the others). There are no
